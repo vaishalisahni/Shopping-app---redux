29880cbac97ae44751717f2046fc3805afa19e22
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-06-28 11:02:04 +0530

    phone view of products improved

--- src/Components/ProductsDisplay.jsx
+++ src/Components/ProductsDisplay.jsx
@@ -7,7 +7,7 @@ function ProductsDisplay() {
   return (
     <div className="flex-1 p-8 bg-gray-50">
       
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {products.map(product => (
           <ProductCard key={product.id} product={product} />
         ))}
